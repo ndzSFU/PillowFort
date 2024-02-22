0326802026f220c7f95faff1cff1412245bdaf97
Noah <ndz@sfu.ca>
2024-02-21 23:15:06 -0800

    Fort maybe

--- src/Model/Fort.java
+++ src/Model/Fort.java
@@ -1,5 +1,21 @@
 package Model;
 
-public class Fort {
+import java.util.ArrayList;
 
+public class Fort {
+    ArrayList<BoardSpot> fortSpots;
+
+    //Not quite sure what to put in this class yet
+    Fort(ArrayList<BoardSpot> selectedSpots){
+        fortSpots = selectedSpots;
+    }
+
+    public int dealDamage(){
+        if(fortSpots.size() == 4 || fortSpots.size() == 5){
+            return 20;
+        } else if(fortSpots.size() == 3){
+            //
+        }
+        return 0;
+    }
 }
