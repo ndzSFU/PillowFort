da8b81225d684d45149413fa575cc0b41c6bfe85
Noah <ndz@sfu.ca>
2024-02-29 21:35:37 -0800

    Game run refactored

--- src/View/GameRunner.java
+++ src/View/GameRunner.java
@@ -7,24 +7,48 @@ import Model.GameBoard;
 public class GameRunner {
     UI userInterface = new UI();
 
-
-
-
     boolean gameDone = false;
     private int oppPoints = 0;
 
-    public void RunGame(GameBoard board, String cheating){
+    private final int oppWinCondition = 2500;
 
-        final int oppWinCondition = 2500;
+    private void printEndGameStats(GameBoard board){
+        System.out.println("Opponents points: " + oppPoints + "/" + oppWinCondition + ".");
+
+        if(board.allFortsDamaged()){
+            System.out.println("Congratulations! You won!");
+        } else{
+            System.out.println("I'm sorry, your fort is all wet! They win!");
+        }
+
+        userInterface.printBoard(board, true);
+        System.out.println("Lower case fort letters are where you shot.");
+    }
+
+    private int getDamageFromForts(GameBoard board){
+        int damageFromForts = 0;
+
+        int fortCounter = 1;
+
+        for(Fort f: board.getForts()){
+            if(f.dealDamage() > 0){
+                System.out.println("Opponent #" + fortCounter + " of " + board.numWorkingForts() + " shot you for " + f.dealDamage() + " points!");
+                fortCounter++;
+                damageFromForts += f.dealDamage();
+            }
+        }
+
+        return damageFromForts;
+    }
+
+    public void RunGame(GameBoard board, String cheating){
 
         boolean cheat = cheating.equals("--cheat");
 
         if(cheat){
             userInterface.printBoard(board, cheat);
         }
-
         userInterface.printBoard(board, gameDone);
-
 
         while(oppPoints < oppWinCondition && !board.allFortsDamaged()){
 
@@ -39,6 +63,7 @@ public class GameRunner {
                 userSpot = userInterface.getUserHit();
                 chosenSpot = board.GetInputtedBoardSpot(userSpot);
             }
+
             chosenSpot.Hit();
 
             if(chosenSpot.isFort()){
@@ -50,32 +75,12 @@ public class GameRunner {
                 System.out.println("Miss.");
             }
 
-            int damageFromForts = 0;
-
-            int fortCounter = 1;
-
-            for(Fort f: board.getForts()){
-                if(f.dealDamage() > 0){
-                    System.out.println("Opponent #" + fortCounter + " of " + board.numWorkingForts() + " shot you for " + f.dealDamage() + " points!");
-                    fortCounter++;
-                    damageFromForts += f.dealDamage();
-                }
-            }
-
-            oppPoints+=damageFromForts;
+            oppPoints+=getDamageFromForts(board);
 
             userInterface.printBoard(board, gameDone);
         }
-
-        System.out.println("Opponents points: " + oppPoints + "/" + oppWinCondition + ".");
-
-        if(board.allFortsDamaged()){
-            System.out.println("Congratulations! You won!");
-        } else{
-            System.out.println("I'm sorry, your fort is all wet! They win!");
-        }
-        gameDone = true;
-        userInterface.printBoard(board, gameDone);
-        System.out.println("Lower case fort letters are where you shot.");
+        printEndGameStats(board);
     }
+
+
 }
